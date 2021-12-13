9b1753a7028aa7482ba9177641d81d5b23ab5c2e
Alan Tavares <alan_tj@hotmail.com.br>
2021-12-13 16:19:17 -0300

    project, components

---
+++ project/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <h1>Teste</h1>
+
+    <br/>
+    <teste-component/>
+</template>
+
+<script>
+
+  import TesteComponent from './components/TesteComponent.vue';
+
+  export default{
+    name:'app',
+    components:{
+      TesteComponent
+    }
+  }
+</script>
---
+++ project/src/components/TesteComponent.vue
@@ -0,0 +1,10 @@
+<template>
+    <h1>Primeiro Componente</h1>
+</template>
+
+<script>
+
+export default {
+   name:"PrimeiroComponente" 
+}
+</script>
